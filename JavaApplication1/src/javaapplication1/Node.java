d384facfc7960deade5067401c6ea13931289997
Vaishali Gupta <vaishalig714@gmail.com>
2018-09-04 19:14:19 +0530

    Update Node.java

--- JavaApplication1/src/javaapplication1/Node.java
+++ JavaApplication1/src/javaapplication1/Node.java
@@ -5,13 +5,27 @@
  */
 package javaapplication1;
 
+import static java.lang.Integer.max;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 import java.util.UUID;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.ShortBufferException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
@@ -28,24 +42,82 @@ public class Node {
     Integer nodeNumber=0;
     String nodeId;
     String referenceNodeId;
-    String childReferenceNodeId;
-    String genesisReferenceNodeId;
+    String[] childReferenceNodeId;
+    static String genesisReferenceNodeId;
     String hashValue;   //Hash Value of the whole set
+    private String encryptedData;
     
-    public Node(float d) 
+    public Node(float d, String ownerName, Integer ownerId) 
     {
         data = d;
         child = null;
         timestamp = new Timestamp(System.currentTimeMillis());
         ++nodeNumber;
         nodeId = UUID.randomUUID().toString();
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+        this.ownerId=ownerId;
+        this.ownerName=ownerName;
+        hashData = generateHashData(data, this.ownerId, this.ownerName);
+        encryptedData = encryption(d, ownerName, ownerId, hashData);
     }
     
+    /* Concept of hash value is same as hashData */
+    
+    static void createGenesisNode(float d, String ownerName, Integer ownerId){
+        Node genesisNode = new Node(d,ownerName,ownerId);
+        genesisReferenceNodeId=genesisNode.nodeId;
+    } 
+    
+    int depthOfTree(Node root)
+{
+    //longest chain finding method
+    
+    // Base case
+    if (root==null)
+        return 0;
+ 
+    // Check for all children and find
+    // the maximum depth
+    int maxdepth = 0;
+    for (int i=0;i<root.child.size();i++)
+        maxdepth = max(maxdepth, depthOfTree(root.child.get(i)));
+ 
+    return maxdepth + 1 ;
+}
+    
+
+    static String encryption(float adata, String ownerName, Integer ownerId, String hashData) {
+        try {
+            byte[] keyBytes=null;
+byte[] ivBytes=null;
+            String data = adata + ownerId + ownerName + hashData;
+            SecretKeySpec key = new SecretKeySpec(keyBytes, "DES");
+            IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+// create the cipher with the algorithm you choose
+// see javadoc for Cipher class for more info, e.g.
+Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+byte[] encrypted= new byte[cipher.getOutputSize(data.getBytes().length)];
+int enc_len = cipher.update(data.getBytes(), 0, data.getBytes().length, encrypted, 0);
+enc_len += cipher.doFinal(encrypted, enc_len);
+return encrypted.toString();
+        } catch (NoSuchPaddingException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BadPaddingException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeyException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidAlgorithmParameterException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ShortBufferException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalBlockSizeException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+       return null;
+    }
     
 
 static float sumNodes(Node root)
@@ -60,11 +132,15 @@ static float sumNodes(Node root)
     return sum;
 }    
 
-static void addNode(Node root, float[] keys){
+//Takes 2 parameters - parent node and values of the children in array to be inserted to that node
+static void addNode(Node root, float[] keys, String[] ownerNames, Integer[] ownerIds){
     if(root==null){
         System.out.println("No node exists");
         return;
     }
+    
+    if(keys.length!=ownerNames.length && ownerIds.length!=keys.length && ownerNames.length!=ownerIds.length)
+        return;
     
     float sum = sumNodes(root);
     for(int i = 0; i<keys.length; i++){
@@ -75,11 +151,23 @@ static void addNode(Node root, float[] keys){
         System.out.println("Condition violated");
     } else{
         root.child=new Vector<>();
-        for(int j = 0; j<keys.length; j++)
-            root.child.add(new Node(keys[j]));
+        root.childReferenceNodeId = new String[keys.length];
+        for(int j = 0; j<keys.length; j++){
+            Node n = new Node(keys[j], ownerNames[j], ownerIds[j]);
+            root.child.add(n);
+            n.referenceNodeId=root.nodeId;
+            n.childReferenceNodeId=null;
+            root.childReferenceNodeId[j]=n.nodeId;
+        }
         System.out.println("Nodes successfully inserted");
     }
 }
 
+    private String generateHashData(float data, Integer ownerId, String ownerName) {
+        String str = String.valueOf(String.valueOf(data).charAt(0)) + String.valueOf(String.valueOf(ownerId).charAt(0)) + String.valueOf(ownerName.charAt(0));
+        return String.valueOf(str.hashCode());
+    }
+
+    
 }
 
